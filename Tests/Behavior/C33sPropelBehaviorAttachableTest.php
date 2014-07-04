2513b17de1adefbc30d913fc18f90311b3818c77
David Herrmann <david@vworld.at>
2014-07-04 21:14:44 +0200

    Made behavior tests work

--- Tests/Behavior/C33sPropelBehaviorAttachableTest.php
+++ Tests/Behavior/C33sPropelBehaviorAttachableTest.php
@@ -11,18 +11,16 @@ class C33sPropelBehaviorAttachableTest extends \PHPUnit_Framework_TestCase
 <database name="testdatabase" defaultIdMethod="native">
     <table name="test_model_without_columns">
         <behavior name="auto_add_pk" />
-        <behavior name="Attachable">
+        <behavior name="c33s_attachable">
             <parameter name="single_columns" value="avatar, icon" />
         </behavior>
     </table>
 </database>
 
 EOF;
-            return;
-            require_once('C33sPropelBehaviorAttachable.php');
-            //$class = new \ReflectionClass('C33sPropelBehaviorAttachable');
+            
             $builder = new \PropelQuickBuilder();
-            $builder->getConfig()->setBuildProperty('behaviorAttachableClass', 'C33sPropelBehaviorAttachable');
+            $builder->getConfig()->setBuildProperty('behaviorC33s_attachableClass', 'C33sPropelBehaviorAttachable');
             $builder->setSchema($schema);
             $con = $builder->build();
         }
@@ -31,12 +29,12 @@ EOF;
     public function testBuildModelWithoutExistingColumns()
     {
         $this->assertTrue(
-            method_exists('TestModelWithoutColumns', 'getAvatar()'),
+            method_exists('TestModelWithoutColumns', 'getAvatar'),
             'Avatar column was not added automatically'
         );
         
         $this->assertTrue(
-            method_exists('TestModelWithoutColumns', 'getIcon()'),
+            method_exists('TestModelWithoutColumns', 'getIcon'),
             'Icon column was not added automatically'
         );
     }
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,11 +8,10 @@ require $autoloadFile;
 
 //set_include_path(__DIR__ . '/../vendor/phing/phing/classes' . PATH_SEPARATOR . __DIR__.'/../Behavior' . PATH_SEPARATOR . get_include_path());
 
-//require_once(__DIR__.'/../Behavior/C33sPropelBehaviorAttachable.php');
 
-$class = new \ReflectionClass('EventDispatcherBehavior');
+//$class = new \ReflectionClass('EventDispatcherBehavior');
 $builder = new \PropelQuickBuilder();
-$builder->getConfig()->setBuildProperty('behavior.event_dispatcher.class', $class->getFileName());
+//$builder->getConfig()->setBuildProperty('behavior.event_dispatcher.class', $class->getFileName());
 $builder->setSchema(file_get_contents(__DIR__.'/../Resources/config/schema.xml'));
 $builder->setClassTargets(array('tablemap', 'peer', 'object', 'query'));
 $builder->build();
